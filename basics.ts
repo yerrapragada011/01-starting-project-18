5c726df70266ab479b87b39e650930e6e24ae234
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-27 13:39:54 -0400

    Functions & Function Types

--- basics.ts
+++ basics.ts
@@ -46,3 +46,12 @@ let course: string | number = 'React - The Complete Guide';
 
 course = 12341;
 
+// Function & types
+
+function add(a: number, b: number) {
+    return a + b;
+}
+
+function print(value: any) {
+    console.log(value);
+}
